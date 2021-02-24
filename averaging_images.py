f45e7bf439a843d41e8b0184bed0130e92125188
Max Heydasch <max@staff-211-171.eduroam.unibe.ch>
2021-02-24 11:47:44 +0100

    initial commit

---
+++ averaging_images.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 14:52:10 2021
+
+@author: max
+"""
+
+from PIL import Image
+import numpy as np
+import os
+import re
+#%%
+path='/Volumes/imaging.data/Max/REF52/DLC_1/SiDLC_43/'
+#%%
+files=os.listdir(path)
+ch1=re.compile('^.*_(?P<Channel1>FRET).tif$')
+ch2=re.compile('^.*_(?P<Channel2>mTFP).tif$')
+
+imList=[]
+for item in files:
+    try:
+        if re.search(ch1, item).group('Channel1')!=None:
+            im=Image.open(os.path.join(path, item))
+            imarray=np.array(im)                        
+            imList.append(imarray)
+            
+    except (AttributeError) as e:
+        print(e)
+        next
+#%%        
+averaged=np.median(np.array(newlist), axis=0)
+tif=Image.fromarray(averaged)
+im.save(os.path.join(path, ('Averaged_'+ item)))
+
+
+#%% Image_substraction
+imagepath='/Volumes/imaging.data/Max/REF52/DLC_1/SiDLC_43/'
+
+for item in files:
+    try:
+        if re.search(ch1, item).group('Channel1')!=None:
+            im=Image.open(os.path.join(path, item))
+            imarray=np.array(im)                        
+            back_subtracted=np.divide(imarray, tif)
+            back_subtracted_tif=Image.fromarray(back_subtracted)
+            back_subtracted_tif.save(os.path.join(path, 'averaged', ('Averaged_'+ item)))
+    except (AttributeError) as e:
+        print(e)
+        next
+
+# =============================================================================
+# im=Image.open(imagepath)
+# imarray=np.array(im)                        
+# #imList.append(imarray)
+# 
+# back_subtracted=np.divide(imarray, tif)
+# back_subtracted_tif=Image.fromarray(back_subtracted)
+# back_subtracted_tif.save('/Users/max/Desktop/test/test.tif')
+# =============================================================================
---
+++ subtract_flatfield.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 11:21:19 2021
+
+@author: max
+"""
+
+from PIL import Image
+import numpy as np
+import os
+import re
+import argparse
+
+#%% Image_substraction
+imagepath='/Volumes/imaging.data/Max/REF52/DLC_1/SiDLC_43/'
+def parseArguments():
+  # Define the parser and read arguments
+  parser = argparse.ArgumentParser(description='subtract a Flatfield image from all images in the folder')
+  parser.add_argument('-d', '--dir', type=str, help='The directory where the images are', required=True)
+  parser.add_argument('-ff', '--flatf', type=str, help='the flatfield image', required =True)
+  args = parser.parse_args()
+  return(args)
+  
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' + directory)
+
+def subtractFlatfield(path, flatfield):
+    ch1=re.compile('^.*_(?P<Channel1>FRET).tif$')
+    ch2=re.compile('^.*_(?P<Channel2>mTFP).tif$')
+    files=os.listdir(path)
+    ff=Image.open(flatfield)
+    flatf=np.array(ff)
+    for item in files:
+        try:
+            if re.search(ch1, item).group('Channel1')!=None:
+                im=Image.open(os.path.join(path, item))
+                imarray=np.array(im)                        
+                back_subtracted=np.divide(imarray, flatf)
+                back_subtracted_tif=Image.fromarray(back_subtracted)
+                saveFolder=createFolder(os.path.join(path, 'flatfield_corrected'))
+                back_subtracted_tif.save(os.path.join(saveFolder, ('Flatcorr_'+ item)))
+        except (AttributeError) as e:
+            print(e)
+            next
+
+
+if __name__ == '__main__':
+    args=parseArguments()
+    path=args.dir
+    flatfield=args.flatf
+    subtractFlatfield(path, flatfield)
+    print(args)
